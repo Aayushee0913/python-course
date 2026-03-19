3c7a98766a1ad24363e871ae6b9ef51f7538fe05
Aayushee0913 <aayusheepoudel980@gmail.com>
2026-03-19 15:01:31 +0545

    day 3 practice

---
+++ day3.py
@@ -0,0 +1,45 @@
+# check if number is positive or negative
+# num = int (input("enter the number"))
+# if num > 0 :
+#     print("positive")
+# elif num < 0 :
+#     print("negative")
+# else :
+#     print("zero")
+# enter age and display "child" if age>13,"teen" if age<20 else "adult"
+
+# age = int(input("Enter age "))
+# if age < 13:
+#     print("Child")
+# elif age < 20:
+#     print("Teen")
+# else:
+#     print("Adult")
+# enter traffic light color and display appropriate message: red=stop, green=go, yellow=get ready
+# color = input("enter the traffic color: ")
+# if color == "red":
+#     print("stop")
+# elif color == "green":
+#     print("go")
+# elif color == "yellow":
+#     print("get ready")
+# else:
+#     print("invalid")
+# check input number is either even or odd
+# num = int (input("enter the number "))
+# if num % 2 == 0:
+#     print("even")
+# else:
+#     print("odd")
+# take marks as input and display grade
+# marks = int (input("enter the marks "))
+# if marks == 80:
+#     print("grade A")
+# elif marks == 70:
+#     print("grade B")
+# elif marks == 60:
+#     print("grade C")
+# elif marks == 50:
+#     print("grade D")
+# else:
+#     print("failed")         
